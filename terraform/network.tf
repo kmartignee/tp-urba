4f96623ff84d3184769404129954d62eabe9f03f
Killian <killianmartignee1406@gmail.com>
2025-06-20 09:30:36 +0200

    Adds Checkov static analysis to CI pipeline
    Enhances the CI pipeline by integrating Checkov, a static analysis tool.
    This addition enables automated security checks on Terraform code,
    identifying potential misconfigurations and vulnerabilities early in
    the development process. Removes SSH key import into AWS.

--- terraform/network.tf
+++ terraform/network.tf
@@ -7,7 +7,6 @@ resource "aws_vpc" "main" {
     Name = "vpc-architecture-3-tiers"
   }
 }
-
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
